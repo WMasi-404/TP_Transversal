1cf2ab111876e85342dc684f3b28b5e7b70e3f0d
WMasi-404 <maximolopez0808@gmail.com>
2026-04-25 19:30:28 -0300

    correccion de pila lifo

--- lista_enlazada/TAD_pila.c
+++ lista_enlazada/TAD_pila.c
@@ -18,15 +18,23 @@ void pila_push(Pila *pila, int valor){//inserta el elemento en la cima
 }
 
 int pila_pop(Pila *pila) {
-  Pila temp = (*pila); //guarda el primer nodo o sea la cima
-  int valor = (temp)->Dato; //guarda el valor que vas a devolver
-  (*pila)   = (*pila)->sig; //mueve la pila al siguiente nodo
-  free(temp);//libera memoria del nodo eliminado
-  return valor;//devuelve el elemento que sacaste
+    if (pila_vacia(pila)) {
+        printf("Error: Pila vacía, no se puede hacer pop\n");
+        return -1; // valor de error
+    }
+    Pila temp = (*pila); //guarda el primer nodo o sea la cima
+    int valor = (temp)->Dato; //guarda el valor que vas a devolver
+    (*pila)   = (*pila)->sig; //mueve la pila al siguiente nodo
+    free(temp);//libera memoria del nodo eliminado
+    return valor;//devuelve el elemento que sacaste
 }
 
 int pila_top(Pila *pila){ //solo mira el valor de la cima
-  return (*pila)->Dato;
+    if (pila_vacia(pila)) {
+        printf("Error: Pila vacía, no hay elemento en la cima\n");
+        return -1; // valor de error
+    }
+    return (*pila)->Dato;
 }
 
 int pila_vacia(Pila *pila){
@@ -35,6 +43,6 @@ int pila_vacia(Pila *pila){
 
 void pila_destruir(Pila *pila){
     while (!pila_vacia(pila)){//recorro la pila mientras sea distntinto de nulp
-        printf("Desapilando %d",pila_pop(pila));
+        printf("Desapilando %d\n",pila_pop(pila));
   }
 }
